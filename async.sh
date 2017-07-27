e5733c21f39533e13e345d553a173b3431580648
SharksevenRo <937257166@qq.com>
2017-07-27 10:38:24 +0800

    regular git config

--- async.sh
+++ async.sh
@@ -6,7 +6,17 @@ if [ ! -n $1 ]; then
 fi
 git status
 git diff
-git config --global user.name "xiongmao"
+
+str = `git remote -v | awk '{print $2}' | head -1 | awk '/^*github.com*/ {print $1}'`
+if [ ! -n "${str}" ]
+then
+    git config --global user.name "SharksevenRo"
+    git config --global user.email "937257166@qq.com"
+else
+    git config --global user.name "xiongmao"
+    git config --global user.email "xiongmao@huanleguang.com"
+fi
+
 echo "please enter commit info："
 
 read msg
