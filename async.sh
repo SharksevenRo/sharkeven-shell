58b0bb98f02b7485a9103b81cdda940892ca0feb
SharksevenRo <937257166@qq.com>
2017-07-28 16:21:50 +0800

    增加回滚add功能

--- async.sh
+++ async.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# 自动跟踪分支 asyc 分支名 自动定位远程库 切换不同用户配置
 if [ ! -n $1 ]; then
 
     git branch --set-upstream-to=origin/$1
@@ -10,19 +9,28 @@ git diff
 str=$(git remote -v | awk '{print $2}' | head -1 | awk '/^*github.com*/ {print $1}')
 if [ ! -z "${str}" ]
 then
+    echo 'github'
     git config --global user.name "SharksevenRo"
     git config --global user.email "937257166@qq.com"
 else
+    echo 'huanleguang'
     git config --global user.name "xiongmao"
     git config --global user.email "xiongmao@huanleguang.com"
 fi
 
-echo "please enter commit info："
+echo "添加提交信息 | Enter 回滚 add "
 
 read msg
 
-git pull origin $1
+if [ ! -z "${msg}" ]
+then
+    git pull origin $1
 
-git add .
-git commit -a -m "$msg"
-git push -u origin $1
+    git add .
+    git commit -a -m "$msg"
+    git push -u origin $1
+else
+        # 撤销提交
+        git reset HEAD .
+
+fi
