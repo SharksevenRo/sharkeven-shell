1e9db10e44fc1af09e3388e8d00e6ff231d9c671
SharksevenRo <937257166@qq.com>
2017-07-28 16:35:49 +0800

    update

--- async.sh
+++ async.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 # git 同步脚本 1 自动跟踪分支提交 2 一键git status git diff 3 自动获取远程库配置 修改用户配置 4 回滚 git add 功能
-branch=$(git branch)
+branch=$(git branch | head -1 | awk '{print $1}')
 if [ ! -n $branch ]; then
-
     git branch --set-upstream-to=origin/$branch
-    git branch --unset-upstream master
 fi
 git status
 git diff
-str=$(git remote -v | awk '{print $2}' | head -1 | awk '/^*github.com*/ {print $branch}')
+str=$(git remote -v | awk '{print $2}' | head -1 | awk '/^*github.com*/ {print $1}')
 if [ ! -z "${str}" ]
 then
-    echo 'github'
+    echo "github ${branch}"
     git config --global user.name "SharksevenRo"
     git config --global user.email "937257166@qq.com"
 else
-    echo 'huanleguang'
+    echo "huanleguang ${branch}"
     git config --global user.name "xiongmao"
     git config --global user.email "xiongmao@huanleguang.com"
 fi
