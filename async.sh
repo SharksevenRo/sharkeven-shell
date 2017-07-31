0ce30755694042b4169f61d02106d462e48a421c
SharksevenRo <937257166@qq.com>
2017-07-31 16:39:37 +0800

    add color support & fix get branch's name bug

--- async.sh
+++ async.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # git 同步脚本 1 自动跟踪分支提交 2 一键git status git diff 3 自动获取远程库配置 修改用户配置 4 回滚 git add 功能
-branch=$(git branch | head -1 | awk '{print $2}')
+
+RED_COLOR='\033[31m'  #红
+GREEN_COLOR='\033[32m' #绿
+YELOW_COLOR='\033[34m' #黄
+RES='\033[0m'
+
+branch=$(git branch | grep '*' | awk '{print $2}')
 if [ ! -n $branch ]; then
 
     git branch --set-upstream-to=origin/$branch
@@ -11,11 +17,11 @@ git diff
 str=$(git remote -v | awk '{print $2}' | head -1 | awk '/^*github.com*/ {print $1}')
 if [ ! -z "${str}" ]
 then
-    echo "github ${branch}"
+    echo -e "${YELOW_COLOR} remote ${RES} ${GREEN_COLOR} github ${RES} ${RED_COLOR} ${branch} ${RES}"
     git config --global user.name "SharksevenRo"
     git config --global user.email "937257166@qq.com"
 else
-    echo "huanleguang ${branch}"
+    echo -e "${YELOW_COLOR} remote ${RES} ${GREEN_COLOR} huanleguang ${RES} ${RED_COLOR} ${branch} ${RES}"
     git config --global user.name "xiongmao"
     git config --global user.email "xiongmao@huanleguang.com"
 fi
